5f2661404d9a5dc002e49954335e75b3f93e2fe0
Lancelot Liu <me@lancy.dev>
2025-02-01 18:56:22 +0000

    feat: Beginnings of application.

--- composeApp/src/desktopMain/kotlin/dev/funky/avocrowdo/App.kt
+++ composeApp/src/desktopMain/kotlin/dev/funky/avocrowdo/App.kt
@@ -1,37 +1,86 @@
 package dev.funky.avocrowdo
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import avocrowdo.composeapp.generated.resources.Res
-import avocrowdo.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
+    var tracePaths by mutableStateOf(false)
+    var colourCode by mutableStateOf(false)
+    var showDirection by mutableStateOf(false)
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Row(
+            Modifier
+                .fillMaxSize()
+                .border(1.dp, MaterialTheme.colors.primary)
+        ) {
+            Column(
+                Modifier
+                    .width(200.dp)
+                    .fillMaxHeight()
+                    .background(MaterialTheme.colors.primary)
+                    .border(1.dp, MaterialTheme.colors.primary),
+            ) {
+                OptionToggle("Trace Paths", tracePaths) { tracePaths = it }
+
+                OptionToggle("Colour Code by Density", colourCode) { colourCode = it }
+
+                OptionToggle("Show as Arrows", colourCode) { colourCode = it }
+                
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+
+            Box(
+                Modifier
+                    .fillMaxWidth()
+            ) {
+                Canvas(Modifier.border(1.dp, MaterialTheme.colors.primary)) {
+                    drawCircle(
+                        color = Color.Red,
+                        radius = 100f,
+                        center = center,
+                        style = Stroke(width = 5f)
+                    )
                 }
             }
+
         }
+    }
+}
+
+@Composable
+fun OptionToggle(
+    title: String,
+    value: Boolean,
+    callback: (Boolean) -> Unit,
+) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(title)
+        Switch(
+            checked = value,
+            onCheckedChange = callback
+        )
     }
 }
--- composeApp/src/desktopMain/kotlin/dev/funky/avocrowdo/main.kt
+++ composeApp/src/desktopMain/kotlin/dev/funky/avocrowdo/main.kt
@@ -1,13 +1,22 @@
 package dev.funky.avocrowdo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Avocrowdo",
     ) {
-        App()
+        window.minimumSize = Dimension(1080, 720)
+
+        Box(Modifier.size(window.width.dp, window.height.dp)) {
+            App()
+        }
     }
 }
