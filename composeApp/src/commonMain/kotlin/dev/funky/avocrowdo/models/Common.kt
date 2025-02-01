c1c8c1fd18633277e29dc18a1b9369aef6aaa23b
LawanF <fathullahlawan@gmail.com>
2025-02-01 18:50:15 +0000

    refactor: turn point into data class for more ergonomic arithmetic

--- composeApp/src/commonMain/kotlin/dev/funky/avocrowdo/models/Common.kt
+++ composeApp/src/commonMain/kotlin/dev/funky/avocrowdo/models/Common.kt
@@ -1,6 +1,13 @@
 package models
 
+import kotlin.math.pow
+
 /**
  * An arbitrary point on the canvas, in the form `(x, y)`.
  */
-typealias Point = Pair<Double, Double>
+data class Point(val x: Double, val y: Double) {
+    fun magnitude(): Double = (this.x.pow(2) + this.y.pow(2)).pow(0.5)
+    operator fun minus(other: Point): Point = Point(this.x - other.x, this.y - other.y)
+    operator fun times(d: Double): Point = Point(this.x * d, this.y * d)
+    operator fun div(d: Double): Point = Point(this.x / d, this.y / d)
+}
